36eb983b39deac1849110579fbefbdd30bf06783
Rafath21 <rafathmirza05@gmail.com>
2021-09-25 03:07:43 +0530

    Commited with new UI

--- src/Reels.jsx
+++ src/Reels.jsx
@@ -1,12 +1,36 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import "./App.css";
+import { firestore } from "./firebase";
 import { AuthContext } from "./AuthProvider";
 import VideoCard from "./VideoCard";
 let Reels = () => {
   let [commentboxOpen, setCommentBoxOpen] = useState(false);
   let [playing, setPlaying] = useState(true);
+  let [feedReels, setFeedReels] = useState([]);
   let value = useContext(AuthContext);
-
-  return <VideoCard value={value} />;
+  useEffect(async () => {
+    let unsubscription = await firestore
+      .collection("users")
+      .doc(value.uid)
+      .collection("reelsFeed")
+      .onSnapshot((querySnapshot) => {
+        setFeedReels(
+          querySnapshot.docs.map((doc) => {
+            return { ...doc.data() };
+          })
+        );
+      });
+    return () => {
+      unsubscription();
+    };
+  }, []);
+  return (
+    <div className="reels-container">
+      {feedReels.map((reel) => {
+        console.log(reel);
+        return <VideoCard value={value} reel={reel} />;
+      })}
+    </div>
+  );
 };
 export default Reels;
